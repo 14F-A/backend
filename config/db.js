3ea9f21aebfd1dd369fbff0e425db3e912657aa8
dernoczidaniel <dernoczi.daniel@students.jedlik.eu>
2023-04-17 08:30:56 +0200

    feltolt

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+const connection = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'ford_salon'
+});
+
+
+
+module.exports = connection;
+
